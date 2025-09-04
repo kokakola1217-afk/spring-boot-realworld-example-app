b983236ac2bb5f69931b95f2b52a6a62d14b1ef5
kokakola1217-afk <kokakola1217@1utar.my>
2025-09-04 14:48:24 +0800

    try ci pipeline

--- src/main/java/io/spring/application/TagsQueryService.java
+++ src/main/java/io/spring/application/TagsQueryService.java
@@ -9,23 +9,24 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class TagsQueryService {
-  private TagReadService tagReadService;
 
-  public List<String> allTags() {
-    return tagReadService.all();
-  }
+    private TagReadService tagReadService;
 
-  public List<String> allTagsSorted(String sortBy) {
-    List<String> tags = tagReadService.all();
-    
-    if ("name_desc".equals(sortBy)) {
-        Collections.sort(tags, Collections.reverseOrder());
-    } else if ("name_asc".equals(sortBy)) {
-        Collections.sort(tags);
-    } else {
-        Collections.sort(tags); 
-    
-    return tags;
-  }
-}
+    public List<String> allTags() {
+        return tagReadService.all();
+    }
+
+    public List<String> allTagsSorted(String sortBy) {
+        List<String> tags = tagReadService.all();
+
+        if ("name_desc".equals(sortBy)) {
+            Collections.sort(tags, Collections.reverseOrder());
+        } else if ("name_asc".equals(sortBy)) {
+            Collections.sort(tags);
+        } else {
+            Collections.sort(tags);
+        }
+
+        return tags;
+    }
 }
